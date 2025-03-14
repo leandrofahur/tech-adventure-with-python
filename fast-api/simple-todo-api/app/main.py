bb291db7cf6c3eb3910e51b5c766f6e5602a356c
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2025-03-13 20:34:36 -0700

    played aroundwith the HTTP and Status code from the FastAPI

--- fast-api/simple-todo-api/app/main.py
+++ fast-api/simple-todo-api/app/main.py
@@ -1,8 +1,43 @@
 from fastapi import FastAPI
-
 app = FastAPI()
+
+TODOS = [
+  {
+    "id": 1,
+    "description": "Buy groceries",
+    "completed": False
+  },
+  {
+    "id": 2,
+    "description": "Walk the dog",
+    "completed": False
+  },
+  {
+    "id": 3,
+    "description": "Pet the cat",
+    "completed": False
+  }
+]
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the Todo API. Use /docs to see the API documentation and /redoc to see the API documentation in a different format."}
+
+@app.get("/todos")
+async def get_all_todos():
+  return {    
+    "todos": TODOS
+  }
+  
+@app.get("/todos/{todo_id}")
+async def get_todo_by_id(todo_id: int):
+  for todo in TODOS:
+    if(todo["id"] == todo_id):
+      return {        
+        "todo": todo
+      }    
+    
+
+
 
