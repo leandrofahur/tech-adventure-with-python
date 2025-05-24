e0cc2ee71a83387339ea930754bb4d66b307eb4c
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-05-24 03:00:49 -0700

    simple todo list with CRUD and Base Moddel

--- fast-api/simple-todo-api/app/main.py
+++ fast-api/simple-todo-api/app/main.py
@@ -1,43 +1,59 @@
-from fastapi import FastAPI
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-TODOS = [
-  {
-    "id": 1,
-    "description": "Buy groceries",
-    "completed": False
-  },
-  {
-    "id": 2,
-    "description": "Walk the dog",
-    "completed": False
-  },
-  {
-    "id": 3,
-    "description": "Pet the cat",
-    "completed": False
-  }
+TASKS = [
+    {'id': '1','title': 'Task One', 'description': 'Description One', 'priority': 'high'},
+    {'id': '2','title': 'Task Two', 'description': 'Description Two', 'priority': 'medium'},
+    {'id': '3','title': 'Task Three', 'description': 'Description Three', 'priority': 'low'},
+    {'id': '4','title': 'Task Four', 'description': 'Description Four', 'priority': 'high'},
+    {'id': '5','title': 'Task Five', 'description': 'Description Five', 'priority': 'medium'},
+    {'id': '6','title': 'Task Six', 'description': 'Description Two', 'priority': 'low'}
 ]
 
+class Task(BaseModel):
+    id: str
+    title: str
+    description: str
+    priority: str
+
+app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Welcome to the Todo API. Use /docs to see the API documentation and /redoc to see the API documentation in a different format."}
+async def get_all_tasks() -> dict:
+    return {"Tasks": TASKS}
 
-@app.get("/todos")
-async def get_all_todos():
-  return {    
-    "todos": TODOS
-  }
-  
-@app.get("/todos/{todo_id}")
-async def get_todo_by_id(todo_id: int):
-  for todo in TODOS:
-    if(todo["id"] == todo_id):
-      return {        
-        "todo": todo
-      }    
+@app.get("/{task_id}")
+async def get_task_by_id(task_id: str) -> dict:
+    for task in TASKS:
+        if task['id'] == task_id:
+            print(task)
+            return {"Tasks": task}
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+@app.post("/")
+async def create_task(task: Task) -> dict:
+    if(task.id in TASKS):
+        raise HTTPException(status_code=400, detail="Task already exists")
+    TASKS.append(task)
+    return {"Tasks": TASKS}
+
+@app.put("/{task_id}")
+async def update_task(task_id: str, updated_task: Task) -> dict:
+    for task in TASKS:
+        if task['id'] == task_id:            
+            task['title'] = updated_task.title
+            task['description'] = updated_task.description
+            task['priority'] = updated_task.priority
+            return {"Task": task}
+    raise HTTPException(status_code=404, detail="Task not found")
     
-
-
+@app.delete("/{task_id}")
+async def delete_task(task_id: str) -> dict:
+    for task in TASKS:
+        if task['id'] == task_id:
+            TASKS.remove(task)
+            return {"Tasks": TASKS}
+    else:
+        raise HTTPException(status_code=404, detail="Task not found")
 
